f474df31e3913408273975e8b57bb51d7f665bcb
Hachem <personal@hachem.cc>
2026-09-02 01:29:46 +0200

    feat: search manpages location

--- src/main.c
+++ src/main.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void)
+int main(int argc, char** argv)
 {
-    (void) puts("type shi");
+    if (argc != 2)
+    {
+        (void) fprintf(stderr, "usage: %s <symbol>\n", argv[0]);
+        return 1;
+    }
+
+    const char* query = argv[1];
+
+    (void) printf("searching for: %s\n", query);
+
+    FILE* pipe = popen("manpath", "r");
+    if (pipe == NULL)
+    {
+        (void) fprintf(stderr, "jafm: failed to run manpath\n");
+        return 1;
+    }
+
+    char buffer[4096];
+    if (fgets(buffer, sizeof(buffer), pipe) == NULL)
+    {
+        (void) fprintf(stderr, "jafm: failed to read manpath\n");
+        (void) pclose(pipe);
+        return 1;
+    }
+
+    (void) pclose(pipe);
+
+    buffer[strcspn(buffer, "\n")] = '\0';
+
+    char* saveptr = NULL;
+    for (char* dir = strtok_r(buffer, ":", &saveptr);
+         dir != NULL;
+         dir = strtok_r(NULL, ":", &saveptr))
+        (void) printf("man dir: %s\n", dir);
+
+    return 0;
 }
